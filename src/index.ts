78164752384ce22595bfc2b823a34860ede45291
Taylor Romero <me@taylorrome.ro>
2023-04-24 20:16:49 -0600

    patch: missed export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as SprucebotLlmFactory } from './bots/SprucebotLlmFactory'
 export { default as SprucebotLlmBotImpl } from './bots/SprucebotLlmBotImpl'
+export { default as SprucebotLlmSkillImpl } from './bots/SprucebotLlmSkillImpl'
 export { OpenAiAdapter } from './bots/adapters/OpenAi'
 export * from './llm.types'
