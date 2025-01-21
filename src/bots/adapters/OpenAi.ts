3d5d00dbd0644c146ae5cdaf4d001a7973110725
Taylor Romero <me@taylorrome.ro>
2025-01-21 08:48:48 -0700

    patch: logging

--- src/bots/adapters/OpenAi.ts
+++ src/bots/adapters/OpenAi.ts
@@ -1,4 +1,5 @@
 import { assertOptions } from '@sprucelabs/schema'
+import { buildLog } from '@sprucelabs/spruce-skill-utils'
 import OpenAI from 'openai'
 import {
     LlmAdapter,
@@ -10,6 +11,7 @@ import OpenAiMessageBuilder from './OpenAiMessageBuilder'
 export class OpenAiAdapter implements LlmAdapter {
     public static OpenAI = OpenAI
     private api: OpenAI
+    private log = buildLog('SprucebotLLM::OpenAiAdapter')
 
     protected constructor(apiKey: string) {
         assertOptions({ apiKey }, ['apiKey'])
@@ -27,10 +29,14 @@ export class OpenAiAdapter implements LlmAdapter {
         const messageBuilder = OpenAiMessageBuilder.Builder(bot)
         const messages = messageBuilder.buildMessages()
 
+        this.log.info('Sending message to OpenAI', JSON.stringify(messages))
+
         const response = await this.api.chat.completions.create({
             messages,
             model: options?.model ?? 'gpt-4o',
         })
+
+        this.log.info('Received response from OpenAI', JSON.stringify(response))
 
         const message =
             response.choices?.[0]?.message?.content?.trim() ??
