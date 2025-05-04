b4435c507a19cf8bba1f6028d9220c4f6a622dcb
Taylor Romero <me@taylorrome.ro>
2025-05-04 14:45:18 -0600

    patch: test fixes

--- src/__tests__/behavioral/adapters/OpenAi.test.ts
+++ src/__tests__/behavioral/adapters/OpenAi.test.ts
@@ -525,7 +525,7 @@ export default class OpenAiTest extends AbstractLlmTest {
 
         const api = `<APIReference>\n\n${descriptions.join('\n\n')}</APIReference>`
 
-        const message = `You have an API available to you to lookup answers. When you need the response of the function call to proceed, you can call a function using a custom markup we created that looks like this: << FunctionName />>. The API will respond with the results and then you can continue the conversation with your new knowledge. If the api call has parameters, call it like this: << FunctionName >>parameters json encoded<</ FunctionName >>. Make sure to json encode the data and drop it between the function tags. I will respond with the api's response and you can use it going forward. The api is as follows (in xml format):\n\n${api}`
+        const message = `You have an API available to you to lookup answers. When you need the response of the function call to proceed, you can call a function using a custom markup we created that looks like this: <<FunctionName/>>. The API will respond with the results and then you can continue the conversation with your new knowledge. If the api call has parameters, call it like this: <<FunctionName>>{{parametersJsonEncoded}}<</FunctionName>>. Make sure to json encode the data and drop it between the function tags. The API is as follows (in xml format):\n\n${api}`
         return { role: 'system', content: message }
     }
 
