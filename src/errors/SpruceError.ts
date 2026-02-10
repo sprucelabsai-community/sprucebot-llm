2b658d9e5bd4583db7f21a8dd989050207d84a80
Taylor Romero <me@taylorrome.ro>
2026-02-10 07:12:42 -0700

    patch: invalid callback guidance improvements

--- src/errors/SpruceError.ts
+++ src/errors/SpruceError.ts
@@ -12,7 +12,16 @@ export default class SpruceError extends BaseSpruceError<ErrorOptions> {
                 break
 
             case 'INVALID_CALLBACK':
-                message = `The callback you tried to invoke (${options.matchedCallback}) is not valid. If you are calling a valid callback, make sure the tag is closed correctly with />> or <</{tagname}>> depending on whether you are passing it arguments. Here is a list of valid callbacks for reference:\n${options.validCallbacks.map((name, idx) => `${idx + 1}: ${name}`).join('\n')}`
+                message = `The callback you tried to invoke (${options.matchedCallback}) failed. Here are common reasons:
+
+1. The callback does not exist (see valid callbacks below).
+2. Syntax error in tags with no arguments. Make sure the callback is in the form of <<callbackName/>> (double << >> and closing forward slash / before the closing >>) and not <<callbackName>>.
+3. Syntax error in tags with arguments. Make sure the callback is in the form of <<callbackName>>{{JSON arguments}}<</callbackName>> and not <<callbackName>>{{JSON arguments}}<</callbackName> (missing closing >> in the opening tag).
+4. Syntax error in the JSON arguments. Make sure the JSON is valid and properly formatted.
+
+Valid callbacks:
+
+${options.validCallbacks.map((name, idx) => `${idx + 1}: ${name}`).join('\n')}`
                 break
 
             case 'CALLBACK_ERROR':
